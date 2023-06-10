252dae83d671b6bfc6f2228bb7db044115fdb04d
Der Melchior <48823437+dermelchior@users.noreply.github.com>
2023-06-10 23:06:40 +0200

    Update README.md

---
+++ lighttpd/files/var/www/html/js/o23_survey_en.js
@@ -0,0 +1,6 @@
+const survey_en = new Survey.Model(json_en);
+survey_en.onComplete.add((sender, options) => {
+    console.log(JSON.stringify(sender.data, null, 3));
+});
+
+$("#surveyElement_en").Survey({ model: survey_en });
---
+++ lighttpd/files/var/www/html/js/o23_umfrage_de.js
@@ -0,0 +1,6 @@
+const survey = new Survey.Model(json_de);
+survey.onComplete.add((sender, options) => {
+    console.log(JSON.stringify(sender.data, null, 3));
+});
+
+$("#surveyElement_de").Survey({ model: survey });
