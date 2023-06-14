9efd2fb66a8589238c58155574510762cf5c47a9
Der Melchior <48823437+DerMelchior@users.noreply.github.com>
2023-06-14 13:06:03 +0200

    Update o23_survey_en.js

--- lighttpd/files/var/www/html/js/o23_survey_en.js
+++ lighttpd/files/var/www/html/js/o23_survey_en.js
@@ -1,4 +1,7 @@
+import { StylesManager } from 'survey-core';
+
 const survey_en = new Survey.Model(json_en);
+survey_en.StylesManager.applyTheme("bootstrap");
 survey_en.onComplete.add((sender, options) => {
     console.log(JSON.stringify(sender.data, null, 3));
 });
