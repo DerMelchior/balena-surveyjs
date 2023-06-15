ae6f7090510ad9c1810ccc3ef45b89627b765c7d
DerMelchior <thilo.haupt@gmail.com>
2023-06-15 13:33:52 +0200

    modified:   lighttpd/files/var/www/html/js/o23_survey_en.js 	modified:   lighttpd/files/var/www/html/js/o23_umfrage_de.js

--- lighttpd/files/var/www/html/js/o23_umfrage_de.js
+++ lighttpd/files/var/www/html/js/o23_umfrage_de.js
@@ -1,6 +1,4 @@
-import { StylesManager } from 'survey-core';
 const survey = new Survey.Model(json_de);
-survey.StylesManager.applyTheme("bootstrap");
 survey.onComplete.add((sender, options) => {
     console.log(JSON.stringify(sender.data, null, 3));
 });
